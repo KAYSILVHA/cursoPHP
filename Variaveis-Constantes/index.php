a570fe1cd9a1dc5ac24b1fa4ab0d424a255806fc
KAYSILVHA <kaysilvha@gmail.com>
2024-01-06 18:14:51 -0300

    variables

--- Variaveis-Constantes/index.php
+++ Variaveis-Constantes/index.php
@@ -10,7 +10,7 @@
 <body>
     <h1>Variáveis e constantes</h1>
     <?php
-    
+
     //variaveis
     $nome="Kayra";
     $sobrenome="Makkena";
@@ -20,6 +20,22 @@
     //constantes
     const PAIS = "Brasil";
     echo "Você mora no " . PAIS;
+
+    //Regras
+    //1 - Variáveis sempre começam com o símbolo $
+
+    //2 - O segundo caractere pode ser letra ou simbolo
+
+    //3 - aceita caracteres [a-z],[A-Z], [0-9] e [_]
+
+    //4 - aceita caracteres da tabela ASCII a partir de 128
+
+    //5 - aceita caracteres acentuados como á, õ, ç
+
+    //6 - A linguagem é case sensitive em relação aos nomes
+
+    //7 - nomes especiais como $this não podem ser usados(palavras reservadas)
+
     ?>
 </body>
 
